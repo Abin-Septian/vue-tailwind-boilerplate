55e144e49e90b025ceb0a9fcb2739aaa66cc17f2
bintang <bintangseptiandaru@gmail.com>
2021-01-03 17:02:01 +0700

    initialize commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="fixed w-full inset-x-0 top-0 bg-blue-400">
+      Test..
+    </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
